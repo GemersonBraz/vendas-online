1141f141325c1f6d9b75761f03f44d322dde237d
GemersonBraz <ggbraz24@gmail.com>
2025-08-10 10:43:30 -0300

    criando um login modular

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import { BackgroundImage } from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+
+    return (<div>
+        <BackgroundImage src="./background.png" />
+        <h1>Login Screen</h1>
+        <p>Welcome to the login screen!</p>
+    </div>);
+
+};
+
+export default LoginScreen;
