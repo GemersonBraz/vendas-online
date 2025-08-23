c88d77d11fcd994a3606cded79054b7228c92493
GemersonBraz <ggbraz24@gmail.com>
2025-08-23 12:46:32 -0300

    config rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
 import './main.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { loginRoutes } from './modules/login/routes';
 
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider, type RouteObject } from "react-router-dom";
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <div>Tela Principal</div>,
+    errorElement: <div>Deu Ruim!</div>,
+  },
+];
+
+// junta todas as rotas
+const router = createBrowserRouter([
+  ...mainRoutes,
+  ...loginRoutes,
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
